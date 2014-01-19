7262aa7bc9f9ff6c565bac7d3242cd93f10ddff3
Sander Verkuijlen <sanderverkuijlen@gmail.com>
2014-01-19 11:05:58 +0100

    Added grunt-contrib-watch to automate tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,16 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
+    //Task configuration
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+//        pkg: grunt.file.readJSON('package.json'),     <-- Not currently needed, may be handy in the future
         uglify: {
             app: {
-                options: {
-//                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> <%= pkg.euthor %> */\n'
-                },
                 files: {
                     'public/app.js': ['static/app/js/**/*.js']
                 }
             }
         },
         concat: {
-            options: {
-//                separator: ';'
-            },
             dist: {
                 src: ['static/vendor/angular.min.js'],
                 dest: 'public/vendor.js'
@@ -24,21 +18,40 @@ module.exports = function(grunt) {
         },
         sass: {
             dist: {
-                options: {
-                },
                 files: {
                     'public/style.css':     'static/app/css/style.scss'
+                }
+            }
+        },
+        watch: {
+            uglify: {
+                files: ['static/app/js/**/*.js'],
+                tasks: ['uglify'],
+                options: {
+                }
+            },
+            concat: {
+                files: ['static/vendor/angular.min.js'],
+                tasks: ['concat'],
+                options: {
+                }
+            },
+            sass: {
+                files: ['static/app/css/style.scss'],
+                tasks: ['sass'],
+                options: {
                 }
             }
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
+    //Load the plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task(s).
+    //Default task: (re-)compile everything (watch must be called manually)
     grunt.registerTask('default', ['uglify', 'concat', 'sass']);
 
 };
