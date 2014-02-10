820a91b6148c50b7f2fbb438bb90372184f84467
Sander Verkuijlen <sanderverkuijlen@gmail.com>
2014-02-10 21:38:13 +0100

    More styling

--- static/app/js/controllers/item.js
+++ static/app/js/controllers/item.js
@@ -1,7 +1,65 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Verkuijlen
- * Date: 27-1-14
- * Time: 18:12
- * To change this template use File | Settings | File Templates.
- */
+app.controller('ItemController', ['$scope', function($scope) {
+
+    $scope.items = [
+        {
+            id:         1,
+            name:       'Paté',
+            amount:     3,
+            checked:    true,
+            users:      [
+                {
+                    id:     1,
+                    name:   'Sander Verkuijlen'
+                },
+                {
+                    id:     2,
+                    name:   'Pieter Willekens'
+                },
+                {
+                    id:     3,
+                    name:   'Paul Sprangers'
+                }
+            ]
+        },
+        {
+            id:         2,
+            name:       'Broodje Paulus',
+            amount:     10,
+            checked:    false,
+            users:      [
+                {
+                    id:     3,
+                    name:   'Paul Sprangers'
+                },
+                {
+                    id:     4,
+                    name:   'Floris Valenkamp'
+                },
+                {
+                    id:     4,
+                    name:   'Thomas Timmers'
+                },
+                {
+                    id:     1,
+                    name:   'Sander Verkuijlen'
+                },
+                {
+                    id:     2,
+                    name:   'Pieter Willekens'
+                }
+            ]
+        },
+        {
+            id:         3,
+            name:       'Kipfilet',
+            amount:     2,
+            checked:    false,
+            users:      [
+                {
+                    id:     5,
+                    name:   'Erwin van der Meijden'
+                }
+            ]
+        }
+    ];
+}]);
